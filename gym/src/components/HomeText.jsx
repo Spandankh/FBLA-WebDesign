f2240ab54d0a33852e8d41fa36711ba9c8b729d8
Special <Isotoxic1@gmail.com>
2023-11-15 13:04:52 -0500

    Added navbar for mobile device, made it more smoother

--- gym/src/components/HomeText.jsx
+++ gym/src/components/HomeText.jsx
@@ -7,10 +7,10 @@ const HomeText = () => {
     const handleClick = () =>{}
     return(
      <div className="text-white font-equinox-sans ">
-        <div className="max-w[500px] mt-[-40px] w-full h-screen mx-auto text-left flex flex-col justify-center px-8">
-            <h1 className="text-2xl font-bold">RANDOM TEXT TEXT TEXT TEXT</h1>
-            <h1 className="text-2xl font-bold">RANDOM TEXT TEXT TEXT TEXT</h1>
-            <p className="text-xs mt-6 mb-4">SAMLLER TEXT TEXT TEXT</p>
+        <div className="max-w[500px] mt-[-150px] w-full h-screen mx-auto text-left flex flex-col justify-center px-8">
+            <h1 className="text-5xl font-bold">RANDOM TEXT TEXT TEXT TEXT</h1>
+            <h1 className="text-4xl font-bold">RANDOM TEXT TEXT TEXT TEXT</h1>
+            <p className="text-2xl mt-6 mb-4">SAMLLER TEXT TEXT TEXT</p>
             <JoinUsButton/>
         </div>
      </div>   
--- gym/src/components/NavBar.jsx
+++ gym/src/components/NavBar.jsx
@@ -1,20 +1,35 @@
-
+import { useState } from 'react';
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
 const NavBar = () => {
+    const[nav, setNav] = useState(true)
+    const handleNav = () => {
+        setNav(!nav)
+    }
     return (
-        <div className="text-white font-equinox-sans">
-            <div className="max-w-auto mx-auto px-2 py-.5 flex items-center justify-between">
-                <h1 className="text-lg font-bold ">WARIO</h1>
-                <ul className="flex space-x-2 text-sm">
-                    <li className="p-2">Home</li>
-                    <li className="p-2">Mission</li>
-                    <li className="p-2">Careers</li>
-                    <li className="p-2">Contract</li>
-                    <li className="p-2">Settings</li>
+        <div className="max-w-auto mx-auto px-4 py-.5 h-24 flex items-center justify-between text-white font-equinox-sans">
+                <h1 className="w-full text-3xl font-bold">WARIO</h1>
+                <ul className="hidden md:flex">
+                    <li className="p-4 ">Home</li>
+                    <li className="p-4 ">Mission</li>
+                    <li className="p-4">Careers</li>
+                    <li className="p-4">Contract</li>
+                    <li className="p-4">Settings</li>
+                </ul>
+            {/*SMALLER NAV BAR */}
+            <div className='flex md:hidden justify-end' onClick={handleNav}>
+                {!nav ? <AiOutlineClose size={20}/> :  <AiOutlineMenu size={20}/>}
+            </div>
+            <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%] top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'}>
+                <h1 className="w-full text-3xl font-bold m-4 ">WARIO</h1>
+                <ul className="pt-24 uppercase">
+                    <li className="p-4 border-b border-gray-600 ">Home</li>
+                    <li className="p-4 border-b border-gray-600">Mission</li>
+                    <li className="p-4 border-b border-gray-600">Careers</li>
+                    <li className="p-4 border-b border-gray-600">Contract</li>
+                    <li className="p-4">Settings</li>
                 </ul>
             </div>
-            {/*RE */}
-
         </div>
     );
 };
