a0c97171198a2f1bf4838ad696e3c822b43c1e19
Special <Isotoxic1@gmail.com>
2023-11-16 18:35:54 -0500

    Added Career Tab

--- gym/src/Pages/Careers.jsx
+++ gym/src/Pages/Careers.jsx
@@ -1,12 +1,58 @@
 import React from "react";
-import TestPng from "../images/gym.png"
 import BoxStyle from "../components/Careers/CareerBox";
 
 const Career = () => {
-    return(
-     <div className="text-black font-equinox-sans bg-[#d6d4d4] bg-cover">
-        <BoxStyle >
-     </div>   
-    )
-}
-export default Career
+  const jobOpening = [{
+    company:"Example Company",
+    position:  "Software Engineer",
+    location: "New York",
+    description: "Conubia mus praesent viverra primis. Sit ipsum sapien condimentum cursus non. Porta cursus, dictumst sagittis morbi feugiat ullamcorper euismod. Urna gravida habitant fringilla sagittis interdum bibendum mi. Maecenas tincidunt tempus ultricies morbi suscipit platea ridiculus egestas placerat rutrum? Ac volutpat mi primis fusce phasellus netus sociis aliquet viverra posuere! Mollis fermentum justo sem nullam et adipiscing augue imperdiet curae; ipsum cursus dictum! Curae; dictumst bibendum molestie, congue.",
+  },
+  {
+    company:"Example Company",
+    position:  "Software Engineer",
+    location: "New York",
+    description: "Conubia mus praesent viverra primis. Sit ipsum sapien condimentum cursus non. Porta cursus, dictumst sagittis morbi feugiat ullamcorper euismod. Urna gravida habitant fringilla sagittis interdum bibendum mi. Maecenas tincidunt tempus ultricies morbi suscipit platea ridiculus egestas placerat rutrum? Ac volutpat mi primis fusce phasellus netus sociis aliquet viverra posuere! Mollis fermentum justo sem nullam et adipiscing augue imperdiet curae; ipsum cursus dictum! Curae; dictumst bibendum molestie, congue.",
+  },
+  {
+    company:"Example Company",
+    position:  "Software Engineer",
+    location: "New York",
+    description: "Conubia mus praesent viverra primis. Sit ipsum sapien condimentum cursus non. Porta cursus, dictumst sagittis morbi feugiat ullamcorper euismod. Urna gravida habitant fringilla sagittis interdum bibendum mi. Maecenas tincidunt tempus ultricies morbi suscipit platea ridiculus egestas placerat rutrum? Ac volutpat mi primis fusce phasellus netus sociis aliquet viverra posuere! Mollis fermentum justo sem nullam et adipiscing augue imperdiet curae; ipsum cursus dictum! Curae; dictumst bibendum molestie, congue.",
+  },
+  {
+    company:"Example Company",
+    position:  "Software Engineer",
+    location: "New York",
+    description: "Conubia mus praesent viverra primis. Sit ipsum sapien condimentum cursus non. Porta cursus, dictumst sagittis morbi feugiat ullamcorper euismod. Urna gravida habitant fringilla sagittis interdum bibendum mi. Maecenas tincidunt tempus ultricies morbi suscipit platea ridiculus egestas placerat rutrum? Ac volutpat mi primis fusce phasellus netus sociis aliquet viverra posuere! Mollis fermentum justo sem nullam et adipiscing augue imperdiet curae; ipsum cursus dictum! Curae; dictumst bibendum molestie, congue.",
+  },
+  {
+    company:"Example Company",
+    position:  "Software Engineer",
+    location: "New York",
+    description: "Conubia mus praesent viverra primis. Sit ipsum sapien condimentum cursus non. Porta cursus, dictumst sagittis morbi feugiat ullamcorper euismod. Urna gravida habitant fringilla sagittis interdum bibendum mi. Maecenas tincidunt tempus ultricies morbi suscipit platea ridiculus egestas placerat rutrum? Ac volutpat mi primis fusce phasellus netus sociis aliquet viverra posuere! Mollis fermentum justo sem nullam et adipiscing augue imperdiet curae; ipsum cursus dictum! Curae; dictumst bibendum molestie, congue.",
+  },
+  {
+    company:"Example Company",
+    position:  "Software Engineer",
+    location: "New York",
+    description: "Conubia mus praesent viverra primis. Sit ipsum sapien condimentum cursus non. Porta cursus, dictumst sagittis morbi feugiat ullamcorper euismod. Urna gravida habitant fringilla sagittis interdum bibendum mi. Maecenas tincidunt tempus ultricies morbi suscipit platea ridiculus egestas placerat rutrum? Ac volutpat mi primis fusce phasellus netus sociis aliquet viverra posuere! Mollis fermentum justo sem nullam et adipiscing augue imperdiet curae; ipsum cursus dictum! Curae; dictumst bibendum molestie, congue.",
+  },
+  {
+    company:"Example Company",
+    position:  "Software Engineer",
+    location: "New York",
+    description: "Conubia mus praesent viverra primis. Sit ipsum sapien condimentum cursus non. Porta cursus, dictumst sagittis morbi feugiat ullamcorper euismod. Urna gravida habitant fringilla sagittis interdum bibendum mi. Maecenas tincidunt tempus ultricies morbi suscipit platea ridiculus egestas placerat rutrum? Ac volutpat mi primis fusce phasellus netus sociis aliquet viverra posuere! Mollis fermentum justo sem nullam et adipiscing augue imperdiet curae; ipsum cursus dictum! Curae; dictumst bibendum molestie, congue.",
+  },
+];
+
+  return (
+    <div className="text-black font-equinox-sans bg-[#f6f7fa] bg-cover">
+        {jobOpening.map((company, index) =>(
+            <BoxStyle key={index} jobOpening={company}/>
+        ))}
+    </div>
+  );
+};
+
+export default Career;
--- gym/src/components/Careers/CareerBox.jsx
+++ gym/src/components/Careers/CareerBox.jsx
@@ -1,18 +1,22 @@
-// JOB CARD
 
-
-const BoxStyle = (CompanyName, jobPostion, jobLocation, jobDescribion) => {
-    return(
-        <div className="w-full py-2 px-4 bg-[#f6f7fa]">
-            <div className="max-w-[1240px] mx-auto grid md:grid-col-3 gap-10 bg-">
-                <div className="border-2 border-gray-600 w-full shadow-xl  outline-2 flex flex-col p-4 rounded-lg ">
-                    <h2 className="text-4xl font-bold text-left pb-auto">{jobPostion}</h2>
-                    <h2 className="text-3xl font-bold text-left">{CompanyName}</h2>
-                    <h2 className="text-2xl font-bold text-left">Location,Location</h2>
-                    <p class="text-2 font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. </p>
-                </div>
-            </div>                
+const BoxStyle = ({ jobOpening }) => {
+    return (
+      <div className="w-full py-2 px-4 bg-[#f6f7fa] ">
+        <div className="max-w-[1240px] mx-auto grid md:grid-col-3 gap-10 bg-">
+          <div className="border-2 border-gray-600 w-full shadow-xl  outline-2 flex flex-col p-4 rounded-lg ">
+            <h2 className="text-4xl font-bold text-left">{jobOpening.position}</h2>
+            <h2 className="text-2xl text-left">{jobOpening.company}</h2>
+            <h2 className="text-2xl text-left">{jobOpening.location}</h2>
+            <p className="text-2 font-light line-clamp-3 pb-1.5">{jobOpening.description}</p>
+            <div className="">
+            <button className="bg-[#164081] text-white max-w-[120px] py-2 px-2 mr-10 whitespace-nowrap font-semibold  tracking-wider hover:bg-[#132c53] transition duration-300 rounded">Apply Now</button>
+            <button className="bg-[#16814f] text-white max-w-[120px] py-2 px-2 whitespace-nowrap font-semibold  tracking-wider hover:bg-[#1e4d29] transition duration-300 rounded">View Job</button>
+            </div>
+          </div>
         </div>
-    )
-}
-export default BoxStyle
+      </div>
+    );
+  };
+  
+  export default BoxStyle;
+  
