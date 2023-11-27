07d074d10f3ccc8a7e5c704ec91c47075cb811a7
Spandankh <spandankhadka2006@gmail.com>
2023-11-26 19:21:53 -0500

    Added stuff

--- server/index.mjs
+++ server/index.mjs
@@ -10,13 +10,22 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/client/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/client/build/index.html"))
+);
 
 // Load the /posts routes
 app.use("/job", job);
+//
+
+
 // Global error handling
 app.use((err, _req, res, next) => {
-  res.status(500).send("Uh oh! An unexpected error occured.", err)
+  res.status(500).send("Uh oh! An unexpected error occured.")
 })
+
 
 // start the Express server
 app.listen(PORT, () => {
